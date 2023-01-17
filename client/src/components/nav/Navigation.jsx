b46c234630fae535f1a53f6a085ca5524ca0dd20
migdalius <30595371+migdalius@users.noreply.github.com>
2023-01-17 19:23:51 +0100

    .

--- client/src/components/nav/Navigation.jsx
+++ client/src/components/nav/Navigation.jsx
@@ -124,7 +124,7 @@ const Navigation = () => {
         <SearchSection>
           <input
             type="text"
-            placeholder="Szukaj..."
+            placeholder="Szukaj...."
             onChange={(e) => setQuery(e.target.value)}
             className="Nav__searchBar"
           />
