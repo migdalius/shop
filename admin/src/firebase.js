c3e4334d6a15e1db2b4e8037d27b4d7e2bf9bfe7
migdalius <30595371+migdalius@users.noreply.github.com>
2023-02-06 11:12:09 +0100

    panel admin (add product)
    - send input dont work, cross orgin problem.

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmHErJAh-se00HMhZl7cOJmisebqO-y7Y",
+  authDomain: "shop-6705b.firebaseapp.com",
+  projectId: "shop-6705b",
+  storageBucket: "shop-6705b.appspot.com",
+  messagingSenderId: "124401792252",
+  appId: "1:124401792252:web:9e65daae4a8a0de0bad379",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
